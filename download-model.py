c4c53a5622ba5fdd6afb674d70fa9457097558ce
Fairuz Hashim <fihashim@gmail.com>
2024-08-30 19:21:42 +0000

    model downloaded from databricks

--- download-model.py
+++ download-model.py
@@ -1,8 +1,11 @@
+import mlflow
 from mlflow.store.artifact.models_artifact_repo import ModelsArtifactRepository
 from mlflow.tracking import MlflowClient
 
+mlflow.set_tracking_uri("databricks")
+
 client = MlflowClient()
-my_model = client.download_artifacts("68baff0203344dfebe89a6c73c6d6cfe", path="model")
+my_model = client.download_artifacts("a7106d297ed4438fa37fe111efa50b7e", path="model")
 print(f"Placed model in: {my_model}")
 
 
